389547dcd3b1edc2127e97a07f7f4d92c225d62f
side menu <vivek65k@gmail.com>
2019-09-25 16:42:54 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,27 @@
 import 'package:flutter/material.dart';
-//page
-import './about.dart';
- void main(){
-   runApp(new MyApp());
- }
+
+//Route Pages
+import 'home/home.dart';
+//import './pages/page2.dart';
+//import 'tabs.dart';
+//import '../pages/home/tab1.dart';
+void main() => runApp(new MyApp());
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      home: new MyHomePage(),
+      routes: <String, WidgetBuilder>{
+//        "/page2": (BuildContext context) => new Page2(),
+        "/home": (BuildContext context) => new HomePage(),
+//        "/detail": (BuildContext context) => new SideTabs(),
+      },
+      initialRoute: "/home",
+      title: 'Flutter Nav',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new HomePage(),
     );
   }
 }
- 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("CBPRo"),
-      ),
-      drawer: new Drawer(
-        child: ListView(
-          children: <Widget>[
-            new UserAccountsDrawerHeader(
-                accountName: new Text("Vivek Pandey"),
-                accountEmail: new Text("vivek65k@gmail.com"),
-              currentAccountPicture: new CircleAvatar(
-                backgroundImage: new NetworkImage('https://i.pravatar.cc/300'),
-              ),
-            ),
-            new ListTile(
-              title: new Text('About Page'),
-              onTap: (){
-                Navigator.push(context,
-                new MaterialPageRoute(builder: (BuildContext)=>new AboutPage()) );
-              },
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
-
